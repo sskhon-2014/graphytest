ff7aa524151c3625a760f568ca60ae2164897d7a
r.kageyama <aredash@gmail.com>
2017-03-13 13:11:54 -0700

    first addition at v2

---
+++ defaults.py
@@ -0,0 +1,5 @@
+file_utr3 = "/Users/DarthRNA/Documents/Robin/genomes/refseq_3utr.bed"
+file_refseq = "/Users/DarthRNA/Documents/Robin/genomes/refseq_names.txt"
+file_targetscan = "/Users/DarthRNA/Documents/Robin/genomes/Targetscan7_fixed.bed"
+file_refgenebed = '/Users/DarthRNA/Documents/Robin/genomes/refGene.bed'
+default_track_folder = "/Users/DarthRNA/Documents/Robin/GRAPHY_TEST/"
